c77574fbd1dde5413a7bc9c2ed2dbff374fa4d8e
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-06 15:04:37 +0200

    sprawdzenie, czy gracz może się poruszać, strzelać, oraz czy ma amunicję na start

--- src/test/java/pang/backend/PlayerTest.java
+++ src/test/java/pang/backend/PlayerTest.java
@@ -2,34 +2,33 @@ package pang.backend;
 
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PlayerTest {
 
     Player player = new Player();
+    SmallBall smallBall = new SmallBall();
+
+    PositionOfPlayer playerPos = new PositionOfPlayer();
+    PositionOfPlayer newPlayerPos = new PositionOfPlayer();
 
     @Test
-    void testPlayerIsLoaded(){
-        assertTrue(player.getHealth()>0);
-        assertTrue(player.getSpeed()>0);
-        assertTrue(player.getDamage()>0);
-        assertTrue(player.isAlive() == true);
-    }
-
-    @Test
-    void testPlayerIsShooting(){
-
+    void testPlayerCanShoot(){
+        player.shoot();
+        assertTrue(smallBall.takeDamage()>0);
     }
 
     @Test
     void testPlayerHasStartingAmmo(){
-
+        assertTrue(player.getAmmo()>0);
     }
 
     @Test
-    void testPlayer(){
-
+    void testPlayerCanMove(){
+        assertTrue(newPlayerPos.changeVertical() != playerPos.vertical);
+        assertTrue(newPlayerPos.changeHorizontal() != playerPos.horizontal);
     }
 
 }
