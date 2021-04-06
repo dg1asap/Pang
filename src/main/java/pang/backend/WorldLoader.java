91b1a9bef648d7974f174165457193117ad651ea
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2021-04-06 14:14:57 +0200

    Podział WorldLoadera na EnemyLoader i PlayerLoader

--- src/main/java/pang/backend/WorldLoader.java
+++ src/main/java/pang/backend/WorldLoader.java
@@ -1,37 +1,12 @@
 package pang.backend;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Queue;
-import java.util.Scanner;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class WorldLoader {
     public static World loadLevel(int levelNumber){
-        Queue <Enemy> enemies = new ArrayBlockingQueue<>(20);
-        Player player = new Player();
-        try{
-            String path = "./data/level/" + levelNumber + ".txt";
-            File file = new File(path);
-            Scanner reader = new Scanner(file);
-            while(reader.hasNextLine()) {
-                String line = reader.nextLine();
-                String[] separatedLine = line.split(" ");
+        Queue <Enemy> enemies = EnemyLoader.loadLevel(levelNumber);
+        Player player = PlayerLoader.load();
 
-                if( separatedLine[0].equals("smallBall") ){
-                    enemies.add( new SmallBall() );
-                }
-
-                if( separatedLine[0].equals("largeBall") ){
-                    enemies.add( new LargeBall() );
-                }
-            }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Level file not found");
-            e.printStackTrace();
-        }
-        return new World(player ,enemies);
-
+        return new World(player, enemies);
     }
 }
