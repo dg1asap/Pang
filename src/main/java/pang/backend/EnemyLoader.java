b5dc200f08691a4e7d5e5d1f4fd06fbef63441e1
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2021-04-06 20:25:12 +0200

    Refactoring EnemyLoader

--- src/main/java/pang/backend/EnemyLoader.java
+++ src/main/java/pang/backend/EnemyLoader.java
@@ -8,31 +8,67 @@ import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class EnemyLoader {
-    public static Queue <Enemy> loadLevel(int levelNumber){
-        Queue <Enemy> enemies = new ArrayBlockingQueue<>(20);
+    static Queue <Enemy> enemies;
+    static int levelNumber;
+    static String path;
 
+    public static Queue <Enemy> loadLevel(int levelNumber) {
         try{
-            String path = "./data/level/" + levelNumber + ".txt";
-            File file = new File(path);
-            Scanner reader = new Scanner(file);
-            while(reader.hasNextLine()) {
-                String line = reader.nextLine();
-                String[] separatedLine = line.split(" ");
-
-                if( separatedLine[0].equals("smallBall") ){
-                    enemies.add( new SmallBall() );
-                }
-
-                if( separatedLine[0].equals("largeBall") ){
-                    enemies.add( new LargeBall() );
-                }
-            }
-            reader.close();
+            selectSource(levelNumber);
+            loadSource();
         } catch (FileNotFoundException e) {
-            System.out.println("Level file not found");
-            e.printStackTrace();
+            logError(e);
         }
 
         return enemies;
     }
+
+    private static void selectSource(int levelNumber){
+        setLevel(levelNumber);
+        updatePath();
+    }
+
+    private static void setLevel(int levelNumber){
+        EnemyLoader.levelNumber = levelNumber;
+    }
+
+    private static void updatePath(){
+        path = "./data/level/" + levelNumber + ".txt";
+    }
+
+    private static void loadSource() throws FileNotFoundException {
+        loadFromFile();
+        loadToEnemies();
+    }
+
+    private static void loadFromFile() throws FileNotFoundException {
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
+        EnemyScanner.loadWithScanner(scanner);
+    }
+
+    private static void loadToEnemies() {
+        enemies = new ArrayBlockingQueue<>(20);
+
+        Iterator names = EnemyScanner.getNamesIterator();
+        Iterator spawnTimes = EnemyScanner.getSpawnTimesIterator();
+        while (names.hasNext() || spawnTimes.hasNext()){
+            String name = (String) names.next();
+            Integer spawnTime = (Integer) spawnTimes.next();
+            loadEnemy(name, spawnTime);
+        }
+    }
+
+    private static void loadEnemy(String name, Integer spawnTime){
+        Enemy enemy = EnemySpawner.spawn(name, spawnTime);
+        enemies.add(enemy);
+    }
+
+    private static void logError(FileNotFoundException e){
+        System.out.println("Level file not found");
+        e.printStackTrace();
+    }
+
 }
+
+
