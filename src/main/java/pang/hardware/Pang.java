2187839889363634964b66e96dd89aba94eac1f7
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-08 18:41:16 +0200

    Przeniesienie testow działania muzyki do klasy AudioTest

--- src/main/java/pang/hardware/Pang.java
+++ src/main/java/pang/hardware/Pang.java
@@ -5,16 +5,6 @@ public class Pang {
 //    Audio audio;
 
     public static void main(String args[]) {
-        loadMusic();
     }
-
-   private static void loadMusic(){
-
-    String musicPath ="./data/music/spaceSample.aif";
-    Audio music = new Audio();
-    music.play(musicPath);
-
-
-   }
 
 }
