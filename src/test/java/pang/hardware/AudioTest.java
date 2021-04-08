d13515d6323c06d6e7bcbdc759d90ac3fb6e0b99
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-08 18:51:29 +0200

    aktualizacja testów, do funkcji programu

--- src/test/java/pang/hardware/AudioTest.java
+++ src/test/java/pang/hardware/AudioTest.java
@@ -1,20 +1,36 @@
 package pang.hardware;
 
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AudioTest {
-    //Audio music = new Audio();
 
-    //@Test
-    //void testMusicIsLoaded(){
-    //    assertTrue(music.getPath() != null);
-    //    assertTrue(music.load() != -1 );
-    //}
-    //@Test
-    //void testMusicInterfaceIsWorking(){
-        //music.play(musicPath);
-        //music.stop();
-       // assertTrue(music.play(musicPath));
-   // }
+    Audio music = new Audio();
+
+    @Test
+    void testMusicIsLoaded(){
+        assertNotNull(music.getPath());
+    }
+
+    @Test
+    void testMusicInterfaceIsWorking(){
+
+        long a,b;
+        music.load();
+        music.loop();
+        JOptionPane.showMessageDialog(null, "Test music");
+        music.pause();
+        a = music.getTime();
+        JOptionPane.showMessageDialog(null, "Music paused");
+        music.resumeLoop();
+        b = music.getTime();
+        JOptionPane.showMessageDialog(null, "Music resumed");
+
+        assertTrue( (a == b) && a != 0);
+
+   }
+   
 }
