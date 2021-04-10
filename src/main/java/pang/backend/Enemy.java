19b40ccaf520cb567b6d30e8f22d10948746d20a
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-10 22:49:11 +0200

    enemy - nowy zamysł konstruktora

--- src/main/java/pang/backend/Enemy.java
+++ src/main/java/pang/backend/Enemy.java
@@ -1,9 +1,26 @@
 package pang.backend;
 
-public abstract class Enemy {
+public abstract class Enemy extends Character {
+    // Movement implementation need to be added //
+
     protected int spawnTime;
+
+    public Enemy(GameConfig config, int spawnTime){
+        this.setHealth(config.getAttribute("health"));
+        this.setDamage(config.getAttribute("damage"));
+        this.setSpeed(config.getAttribute("speed"));
+
+        this.spawnTime = spawnTime;
+    }
 
     Enemy(int spawnTime){
         this.spawnTime = spawnTime;
+    }
+
+    void takeDamage(double damage){
+        //Not implemented yet
+    }
+    void attack(){
+        //Not implemented yet
     }
 }
