0a0b1ad8cee676aefd681441bbf043795da459d8
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-10 12:34:19 +0200

    Wczytywanie playera przez interfejs GameConfig

--- src/main/java/pang/backend/Player.java
+++ src/main/java/pang/backend/Player.java
@@ -1,7 +1,20 @@
 package pang.backend;
 
-public class Player  {
-    public boolean isAlive(){
-        return true;
+public class Player extends Character implements Movement {
+
+    int ammoAmount;
+    CharacterPosition position;
+
+    public Player(GameConfig config){
+        this.health = config.getAttribute("health");
+        this.damage = config.getAttribute("damege");
+        this.speed = config.getAttribute("speed");
+
+        ammoAmount = config.getAttribute("ammunition");
+
+        position.setHorizontal(config.getAttribute("startX"));
+        position.setVertical(config.getAttribute("startY"));
     }
+
+
 }
