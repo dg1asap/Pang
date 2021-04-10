495f64d2d282b4086c2a444745817cd6a0c7bc0f
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-10 13:31:45 +0200

    aktualizacja playera

--- src/main/java/pang/backend/Player.java
+++ src/main/java/pang/backend/Player.java
@@ -3,18 +3,49 @@ package pang.backend;
 public class Player extends Character implements Movement {
 
     int ammoAmount;
-    CharacterPosition position;
+    double gravityForce;
+
 
     public Player(GameConfig config){
         this.health = config.getAttribute("health");
-        this.damage = config.getAttribute("damege");
+        this.damage = config.getAttribute("damage");
         this.speed = config.getAttribute("speed");
 
         ammoAmount = config.getAttribute("ammunition");
+        gravityForce = config.getAttribute("gravity");
 
-        position.setHorizontal(config.getAttribute("startX"));
-        position.setVertical(config.getAttribute("startY"));
+        this.position.setHorizontal(config.getAttribute("startX"));
+        this,position.setVertical(config.getAttribute("startY"));
     }
 
+    void shoot(){
+        ammoAmount += -1;
+    }
+
+    int getAmmoAmount(){
+        return ammoAmount;
+    }
+
+    double getYPosition(){
+        return position.getVertical();
+    }
+
+    double getXPosition(){
+        return position.getHorizontal();
+    }
+
+    public void changeVertical(){
+        //Not implemented yet
+    }
+
+    public void changeHorizontal(){
+        //Not implemented yet
+    }
+
+
+    public boolean isCollision() {
+        return false;
+    }
+    
 
 }
