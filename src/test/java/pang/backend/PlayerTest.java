0467841050fb13f93a5d61c4075cfc0262d0b3bf
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-10 13:04:01 +0200

    Testy sprawdzające, czy gracz ma amunicję, oraz czy może się poruszać

--- src/test/java/pang/backend/PlayerTest.java
+++ src/test/java/pang/backend/PlayerTest.java
@@ -1,6 +1,5 @@
 package pang.backend;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,37 +7,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
-    Player player = new Player();
-    SmallBall smallBall = new SmallBall();
-
-    PositionOfPlayer playerPos = new PositionOfPlayer();
-    PositionOfPlayer newPlayerPos = new PositionOfPlayer();
-    GamePlayWindow gameWindow = new GamePlayWindow();
-
-    @Test
-    void testPlayerCanShoot(){
-        player.shoot();
-        assertTrue(smallBall.takeDamage()>0);
-    }
+    GameConfig config = new GameConfig();
+    Player player = new Player(config);
+    CharacterPosition position;
 
     @Test
     void testPlayerHasStartingAmmo(){
-        assertTrue(player.getAmmo()>0);
+        assertTrue(player.getAmmoAmount()>0);
     }
 
     @Test
     void testPlayerCanMove(){
-        assertTrue(newPlayerPos.changeVertical() != playerPos.vertical);
-        assertTrue(newPlayerPos.changeHorizontal() != playerPos.horizontal);
+        double firstYPosition = position.getVertical();
+        double firstXPosition = position.getHorizontal();
+
+        player.changeHorizontal();
+        player.changeVertical();
+
+        assertTrue(isCharacterPositionDifferent(firstXPosition,firstYPosition,player.getXPosition(), player.getYPosition()));
+
+    }
+
+    private boolean isCharacterPositionDifferent(double startX, double startY, double endX, double endY){
+        return isXPositionDifferent(startX, endX) && isYPositionDifferent(startY, endY);
+    }
+
+    private boolean isXPositionDifferent(double startX, double endX){
+        return startX != endX;
+    }
+
+    private boolean isYPositionDifferent(double startY, double endY){
+        return startY != endY;
     }
 
 
-    @Test
-    void testPlayerCollisions(){
-        playerPos.changeVertical();
-        assertTrue(player.isCollision() > 0 ); //kolizja z lewą stroną ekranu
 
-        playerPos.changeVertical();
-        assertTrue(player.isCollision() <= gameWindow.maxSize()); //kolizja z prawą stroną ekranu
-    }
+
+ //   SmallBall smallBall = new SmallBall();
+//
+ //   CharacterPosition playerPos = new CharacterPosition();
+ //   CharacterPosition newPlayerPos = new CharacterPosition();
+ //   GamePlayWindow gameWindow = new GamePlayWindow();
+
+ //   @Disabled
+//    void testPlayerCanShoot(){
+ //       player.shoot();
+//        assertTrue(smallBall.takeDamage()>0);
+ //   }
+
+
+ //   @Disabled
+ //   void testPlayerCollisions(){
+  //      playerPos.changeVertical();
+ //       assertTrue(player.isCollision() > 0 ); //kolizja z lewą stroną ekranu
+//
+  //      playerPos.changeVertical();
+  //      assertTrue(player.isCollision() <= gameWindow.maxSize()); //kolizja z prawą stroną ekranu
+  //  }
 }
