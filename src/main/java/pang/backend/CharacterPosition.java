56212425222e495987f371ad1cc46533e3b5f045
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-10 12:36:53 +0200

    Ustawienie współrzędnych dla bohatera gry

---
+++ src/main/java/pang/backend/CharacterPosition.java
@@ -0,0 +1,14 @@
+package pang.backend;
+
+public class CharacterPosition {
+    double vertical;
+    double horizontal;
+    
+    void setVertical(double posY){
+        this.vertical = posY;
+    }
+
+    void setHorizontal(double posX){
+        this.horizontal = posX;
+    }
+}
