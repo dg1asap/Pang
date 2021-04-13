6eeaa150a698b51db245a530e35ec2095927dd0f
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-04-13 12:05:16 +0200

    implementacja wczytywania MegaBall

--- src/main/java/pang/backend/EnemyFactory.java
+++ src/main/java/pang/backend/EnemyFactory.java
@@ -7,19 +7,23 @@ import java.nio.file.Path;
 public class EnemyFactory {
     GameConfig smallBallConfig;
     GameConfig largeBallConfig;
+    GameConfig megaBallConfig;
 
     EnemyFactory(Path configPath) throws ConfigNotFoundException {
         ConfigLoader smallBallConfigLoader = new ConfigLoader(configPath);
         ConfigLoader largeBallConfigLoader = new ConfigLoader(configPath);
+        ConfigLoader megaBallConfigLoader = new ConfigLoader(configPath);
 
         smallBallConfig = smallBallConfigLoader.getConfig("SmallBall");
         largeBallConfig = largeBallConfigLoader.getConfig("LargeBall");
+        megaBallConfig = megaBallConfigLoader.getConfig("MegaBall");
     }
 
     public Enemy create(String name, Integer spawnTime) throws IllegalArgumentException{
         return switch (name) {
             case "smallBall" -> new SmallBall(smallBallConfig, spawnTime);
             case "largeBall" -> new LargeBall(largeBallConfig, spawnTime);
+            case "megaBall" -> new MegaBall(largeBallConfig, spawnTime);
             default -> throw new IllegalArgumentException("Enemy name not found");
         };
     }
