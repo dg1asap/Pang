38483ec765e816125bbfb3e8b18df75671299f4e
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-03 14:47:18 +0200

    dodanie obsługi klawiatury

--- src/main/java/pang/backend/character/player/Player.java
+++ src/main/java/pang/backend/character/player/Player.java
@@ -3,13 +3,17 @@ package pang.backend.character.player;
 import pang.backend.character.Character;
 import pang.backend.character.Movement;
 import pang.backend.config.GameConfig;
+import pang.frontend.GamePanel;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Player extends Character implements Movement {
 
     int ammoAmount;
     double gravityForce;
-    double dx = 1;
-    double dy = 1;
+    int dx;
+    int dy;
 
     public Player(GameConfig config){
 
@@ -21,29 +25,66 @@ public class Player extends Character implements Movement {
         this.setPosY(config.getAttribute("startPosY"));
     }
 
+    public void keyPressed(KeyEvent e){
+        if(e.getKeyCode()==KeyEvent.VK_W){
+            if(this.y == GamePanel.getGameHeight() - this.height) {
+                setY(-(int) this.speed);
+            }
+        } //do zmiany na skok
+        else if(e.getKeyCode()==KeyEvent.VK_A){
+            setX(-(int)this.speed);
+        }
+        else if(e.getKeyCode()==KeyEvent.VK_D){
+            setX((int)this.speed);
+        }
+    }
+
+    public void keyReleased(KeyEvent e){
+        if(e.getKeyCode()==KeyEvent.VK_W){
+            setY((int)gravityForce);
+        }
+        else if(e.getKeyCode()==KeyEvent.VK_A){
+            setX(0);
+        }
+        else if(e.getKeyCode()==KeyEvent.VK_D){
+            setX(0);
+        }
+    }
+
+    public void setX(int dx){
+        this.dx = dx;
+    }
+
+    public void setY(int dy){
+        this.dy = dy;
+    }
+
+    public void move(){
+        x = x + dx;
+        y = y + dy;
+    }
+
+
+
+    public void draw(Graphics g){
+        g.setColor(Color.red);
+        g.fillRect(x,y,(int)width ,(int)height);
+    }
+
     void shoot(){
         ammoAmount += -1;
         //Not implemented yet
     }
 
-    int getAmmoAmount(){
+    public int getAmmoAmount(){
         return ammoAmount;
     }
 
-    double getGravityForce(){
+    public double getGravityForce(){
         return gravityForce;
     }
 
 
-    @Override
-    public void changeYDirection(){
-        this.changePosY(dy);
-    }
-
-    @Override
-    public void changeXDirection(){
-        this.changePosX(dx);
-    }
 
     @Override
     public boolean isCollision() {
