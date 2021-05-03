aa41be0d4a800bf5ae1e0f58d1ab3afe2ac9b2d8
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-03 14:47:00 +0200

    main + gui

--- src/main/java/pang/hardware/Pang.java
+++ src/main/java/pang/hardware/Pang.java
@@ -1,10 +1,18 @@
 package pang.hardware;
 
+import pang.backend.config.ConfigLoader;
+import pang.backend.config.GameConfig;
+import pang.frontend.Screen;
+import pang.frontend.ScreenTEST;
+
+import java.nio.file.Path;
+
 public class Pang {
 //    Screen screen;
 //    Audio audio;
 
     public static void main(String args[]) {
+        new Screen();
     }
 
 }
