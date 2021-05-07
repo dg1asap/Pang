393ad0fcb05ed22bde854113929172d80fbeaa57
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-07 15:43:02 +0200

    panel poprzedzający wejście do gry, wczytuje nick użytkownika

---
+++ src/main/java/pang/gui/UserDataPanel.java
@@ -0,0 +1,27 @@
+package pang.gui;
+
+import pang.hardware.Screen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UserDataPanel extends PangPanel {
+    public UserDataPanel(Screen screen) {
+        JTextField userNickname = new JTextField();
+        userNickname.setPreferredSize(new Dimension(200,50));
+
+        JLabel inputDataText= new JLabel("Please input your nick");
+
+
+        JButton okButton = createButtonToChangeWindowTo("Submit","Gameplay", screen);
+        JButton cancelButton = createButtonToChangeWindowTo("Cancel","Menu", screen);
+
+        add(inputDataText);
+        add(userNickname);
+        add(okButton);
+        add(cancelButton);
+
+    }
+}
+
+
