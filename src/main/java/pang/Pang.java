c042734120365503e2f53915a6cb1a0b5c6d0e72
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-07 15:46:18 +0200

    tworzenie audio poza Screenem

--- src/main/java/pang/Pang.java
+++ src/main/java/pang/Pang.java
@@ -1,12 +1,19 @@
 package pang;
 
+import pang.gui.PanelCreator;
+import pang.hardware.Audio;
 import pang.hardware.Screen;
 
 public class Pang {
     private Screen screen;
+    private Audio music;
 
     Pang(){
-        this.screen = new Screen();
+        screen = new Screen();
+        music = new Audio();
+
+        PanelCreator panelCreator = new PanelCreator(screen, music);
+        screen.setPanelCreator(panelCreator);
     }
 
 }
