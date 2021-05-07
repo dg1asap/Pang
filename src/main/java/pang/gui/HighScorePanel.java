e2231d0b3886de7ff69ea34c82ed3eb5bcb7d027
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-07 15:42:25 +0200

    panel odpowiedzialny za wyświetlanie najlepszych wyników

---
+++ src/main/java/pang/gui/HighScorePanel.java
@@ -0,0 +1,14 @@
+package pang.gui;
+
+import pang.hardware.Screen;
+
+import javax.swing.*;
+
+public class HighScorePanel extends PangPanel {
+    public HighScorePanel(Screen screen) {
+        JButton backButton = createButtonToChangeWindowTo("Back","Menu", screen);
+        JLabel highScore = new JLabel("Best players:");
+        add(highScore);
+        add(backButton);
+    }
+}
