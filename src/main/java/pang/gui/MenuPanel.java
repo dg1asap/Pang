2724c5df7a7daaebe48f0738075418b8cf3e257a
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-05-07 15:46:38 +0200

    panel menu głównego

--- src/main/java/pang/gui/MenuPanel.java
+++ src/main/java/pang/gui/MenuPanel.java
@@ -6,9 +6,18 @@ import javax.swing.*;
 
 public class MenuPanel extends PangPanel {
     MenuPanel(Screen screen) {
-        JButton playButton = createButtonToChangeWindowTo("Play","Gameplay", screen);
+        JButton playButton = createButtonToChangeWindowTo("Play","UserData", screen);
+        JButton levelButton = createButtonToChangeWindowTo("Level", "Level", screen);
+        JButton scoreButton = createButtonToChangeWindowTo("High scores", "HighScores", screen);
         JButton settingsButton = createButtonToChangeWindowTo("Settings","Settings", screen);
+
+        JButton quitButton = new JButton("QUIT");
+        quitButton.addActionListener(e -> System.exit(0));
+
         add(playButton);
+        add(levelButton);
+        add(scoreButton);
         add(settingsButton);
+        add(quitButton);
     }
 }
