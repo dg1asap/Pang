eba18631e028a217f63127553dbf7e6c68a439db
Damian Górski <d.gorski.1@stud.elka.pw.edu.pl>
2021-06-04 20:26:03 +0200

    Uwzględenienie rozmiaru obietku przy wyliczaniu jego zasiegu poruszania się

--- src/main/java/pang/backend/world/WorldBorder.java
+++ src/main/java/pang/backend/world/WorldBorder.java
@@ -3,11 +3,13 @@ package pang.backend.world;
 import pang.backend.util.PangVector;
 import pang.backend.character.Character;
 
+import java.awt.geom.RectangularShape;
+
 public class WorldBorder {
-    private int leftBorder;
-    private int rightBorder;
-    private int topBorder;
-    private int bottomBorder;
+    private final int leftBorder;
+    private final int rightBorder;
+    private final int topBorder;
+    private final int bottomBorder;
 
     public WorldBorder(PangVector oppositeCorner) {
         this.rightBorder = oppositeCorner.getX();
@@ -17,28 +19,34 @@ public class WorldBorder {
     }
 
     public boolean isInBorderOfWorld(Character character, String direction, int stepLength) {
-        PangVector characterPosition = character.getPosition();
         if ( direction.equals("posX"))
-            return !isBehindRightBorder(characterPosition, stepLength) && !isBehindLeftBorder(characterPosition, stepLength);
+            return !isBehindRightBorder(character, stepLength) && !isBehindLeftBorder(character, stepLength);
         else
-            return !isBehindTopBorder(characterPosition, stepLength) && !isBehindBottomBorder(characterPosition, stepLength);
+            return !isBehindTopBorder(character, stepLength) && !isBehindBottomBorder(character, stepLength);
     }
 
-    private boolean isBehindLeftBorder(PangVector position, int stepLength) {
+    private boolean isBehindLeftBorder(Character character, int stepLength) {
+        PangVector position = character.getPosition();
         return position.getX() + stepLength < leftBorder;
     }
 
-    private boolean isBehindRightBorder(PangVector position, int stepLength) {
-        return position.getX() + stepLength + 42 > rightBorder;
+    private boolean isBehindRightBorder(Character character, int stepLength) {
+        PangVector position = character.getPosition();
+        RectangularShape hitBox = character.getHitBox();
+        double width = hitBox.getWidth();
+        return position.getX() + stepLength + width > rightBorder;
     }
 
-    private boolean isBehindTopBorder(PangVector position, int stepLength) {
+    private boolean isBehindTopBorder(Character character, int stepLength) {
+        PangVector position = character.getPosition();
         return position.getY() + stepLength < topBorder;
     }
 
-    private boolean isBehindBottomBorder(PangVector position, int stepLength) {
-        return position.getY() + stepLength  > bottomBorder;
-
+    private boolean isBehindBottomBorder(Character character, int stepLength) {
+        PangVector position = character.getPosition();
+        RectangularShape hitBox = character.getHitBox();
+        double height = hitBox.getHeight();
+        return position.getY() + stepLength + height > bottomBorder;
     }
 
 }
