e94adef36851bd193f087ba234fb05ebcbaab5b1
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-06-14 02:05:47 +0200

    klasa serwera odpowiedzialna za kontrolę i obsługę żądań użytkownika

---
+++ server/src/MessageHandler.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.Socket;
+
+public class MessageHandler {
+    private String clientCommand;
+
+    public MessageHandler(String clientCommand){
+        this.clientCommand = clientCommand;
+    }
+
+    public void handleCommand(DataOutputStream dataOutputStream){
+        switch (clientCommand){
+            case "getHighScores" -> DataSender.sendData(dataOutputStream,"highScores");
+            case "getMaps" -> DataSender.sendData(dataOutputStream,"levels");
+            case "getConfigs" -> DataSender.sendData(dataOutputStream, "config");
+            case "logout" -> {
+                try {
+                    Thread.sleep(1000);
+                    //System.err.println("Client disconnected");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            default -> System.out.println("Command not available [" + clientCommand + "]" );
+        }
+
+    }
+}
