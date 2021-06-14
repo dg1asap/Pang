b3c3e3a02ebe9de3b3f2bd1717d620f27484b226
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-06-14 02:05:09 +0200

    klasa serwera odpowiedzialna za wysyłanie odpowiedzi do klienta

---
+++ server/src/DataSender.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.nio.file.Path;
+
+
+public class DataSender {
+
+    public static void sendData(DataOutputStream dataOutputStream, String dataName) {
+        try {
+            File serverFiles = Path.of("server", "data", dataName).toFile();
+            int numberOfFilesToSend = serverFiles.listFiles().length;
+
+            File[] files = serverFiles.listFiles();
+
+            dataOutputStream.writeInt(numberOfFilesToSend);
+
+            for (File file : files) {
+                FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile());
+
+                byte[] fileData = new byte[(int) file.length()];
+                fileInputStream.read(fileData);
+
+                dataOutputStream.writeInt(file.getName().getBytes().length);
+                dataOutputStream.write(file.getName().getBytes());
+
+                dataOutputStream.writeInt(fileData.length);
+                dataOutputStream.write(fileData);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
