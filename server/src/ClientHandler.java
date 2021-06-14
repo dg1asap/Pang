b2d7d71d2724271a1feed0dfd73b4f1f40d650f9
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-06-14 02:07:40 +0200

    klasa tworząca wątek dla każdego klienta

--- server/src/ClientHandler.java
+++ server/src/ClientHandler.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.Socket;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable,Serializable{
 
     private Socket socket;
 
@@ -11,24 +11,50 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run() {
-        try {
-            while (true) {
+            try {
+                boolean isRunning = true;
+                while(isRunning) {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    DataOutputStream dataOutputStream =  new DataOutputStream(socket.getOutputStream());
+                    String clientCommand = bufferedReader.readLine();
 
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintStream printStream = new PrintStream(socket.getOutputStream());
-                String clientCommand = bufferedReader.readLine();
+                    if ((clientCommand) != null) {
+                        if(clientCommand.contains("saveScore")){
+                            String[] userData = clientCommand.split("\\s+");
 
-                if(clientCommand != null) {
-                    System.out.println(clientCommand);
+                            String nick = userData[1];
+                            String levelName = userData[2];
+                            Double score = Double.parseDouble(userData[3]);
+                            new DataSaver(levelName,score,nick).save();
+                            socket.close();
+                            Server.removeClientNumber();
+                            isRunning = false;
+                        }
+                        else{
+                            MessageHandler messageHandler = new MessageHandler(clientCommand);
+                            messageHandler.handleCommand(dataOutputStream);
+
+                            if(clientCommand.equals("wait")){
+                                socket.close();
+                                Server.removeClientNumber();
+                                isRunning = false;
+                            }
+                            else if (clientCommand.equals("logout")) {
+                                bufferedReader.close();
+                                dataOutputStream.close();
+                                socket.close();
+                                Server.removeClientNumber();
+                                isRunning = false;
+                            }
+                            else {
+                                System.out.println("\u001B[36m" + "Request handled" + "\u001B[0m");
+                            }
+                        }
+                    }
                 }
-
-                bufferedReader.close();
-                printStream.close();
-                socket.close();
-                Server.removeClientNumber();
+            } catch (Exception e) {
+                System.err.println("Client error");
             }
-        } catch (Exception e) {
-            System.err.println("Client disconnected");
-        }
+
     }
 }
