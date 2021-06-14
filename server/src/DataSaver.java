944a92b95bba3a28a9e0d31ecfd11c73c8c6958a
Michał Ogrodnik <m.ogrodnik@stud.elka.pw.edu.pl>
2021-06-14 02:08:35 +0200

    klasa odpowiedzialna za zapis otrzymanego wyniku od klienta  do pliku na serwerze

---
+++ server/src/DataSaver.java
@@ -0,0 +1,40 @@
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class DataSaver {
+    private String levelName;
+    private Double score;
+    private String nick;
+
+    public DataSaver(String levelName, Double score, String nick) {
+        this.levelName = levelName;
+        this.score = score;
+        this.nick = nick;
+    }
+
+    public void save() {
+        File highScoresFile = Path.of("server","data", "highScores", levelName + ".txt").toFile();
+        try{
+            if(highScoresFile.exists()){
+                FileWriter scoreWriter = new FileWriter(highScoresFile, true);
+                scoreWriter.write("\n" + nick + " " + score.intValue());
+                scoreWriter.close();
+                System.out.println("Added score to file: " + levelName + ".txt");
+            }
+            else{
+                FileWriter scoreWriter = new FileWriter(highScoresFile, true);
+                scoreWriter.write(nick + " " + score.intValue());
+                scoreWriter.close();
+                System.out.println("Created new HighScores file: " + levelName + ".txt");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
